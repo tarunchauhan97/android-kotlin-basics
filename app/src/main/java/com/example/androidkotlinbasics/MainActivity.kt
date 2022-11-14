fd8bb0e939b10a1eb585082c6bcbd70698f45fe1
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-11-14 11:12:48 +0530

    12 webview in application

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.androidkotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()//to open webview in application itself
+
+        webView.loadUrl("https://www.facebook.com/")
     }
+
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+
+        }
+
+    }
+
 }
