f7932da65e10548f95e11d3cb276c3e94309931d
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-11-17 15:07:04 +0530

    bindings of variable avoiding findviewbyid

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androidkotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidkotlinbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.button5.setOnClickListener {
+
+            mainBinding.textView2.text = mainBinding.editTextTextPersonName2.text.toString()
+
+        }
+
     }
 }
