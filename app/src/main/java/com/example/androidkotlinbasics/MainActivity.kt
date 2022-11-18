53bb313288d72c602a0842568c5d0bce7404f8d1
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-11-18 16:17:14 +0530

    supporting different pixel densities

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setLogo(R.mipmap.logo_bird)
+        supportActionBar!!.setDisplayUseLogoEnabled(true)
+        
         setContentView(R.layout.activity_main)
     }
 }
