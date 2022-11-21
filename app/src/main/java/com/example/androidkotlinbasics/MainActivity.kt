b8518164419e98728fe5d494a6db7d8d4ed05a94
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-11-21 08:31:50 +0530

    replace 1 to 2 fragment in same activity

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.androidkotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var replace :Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replace = findViewById(R.id.buttonReplace)
 
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
@@ -18,7 +24,17 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.add(R.id.frame, firstFragment)
         fragmentTransaction.commit()
 
+        replace.setOnClickListener {
 
+            val secondFragmentManager:FragmentManager = supportFragmentManager
+            val secondFragmentTransaction :FragmentTransaction = secondFragmentManager.beginTransaction()
+
+            val secondFragment = SecondFragment()
+
+            secondFragmentTransaction.replace(R.id.frame,secondFragment)
+            secondFragmentTransaction.commit()
+
+        }
 
     }
 }
