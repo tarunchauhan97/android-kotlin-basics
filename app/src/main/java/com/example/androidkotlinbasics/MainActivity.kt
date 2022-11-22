6729705276452d164ef2ed6454f88a04a7aea235
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-11-22 17:20:03 +0530

    android one to second screen error

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.androidkotlinbasics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName: TextInputEditText
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPhone: TextInputEditText
+    lateinit var signUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etName = findViewById(R.id.editTextName)
+        etEmail = findViewById(R.id.editTextEmail)
+        etPhone = findViewById(R.id.editTextPhone)
+        signUp = findViewById(R.id.buttonSignUp)
+
+        signUp.setOnClickListener {
+            val name = etName.text.toString()
+            val email = etEmail.text.toString()
+            val phone = etPhone.text.toString().toLong()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("namess", name)
+//            intent.putExtra("emailss", email)
+//            intent.putExtra("phoness", phone)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidkotlinbasics/SecondActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidkotlinbasics
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity : AppCompatActivity() {
+
+    lateinit var tvName: TextView
+    lateinit var tvEmail: TextView
+    lateinit var tvPhone: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        tvName = findViewById(R.id.textViewName)
+        tvEmail = findViewById(R.id.textViewEmail)
+        tvPhone = findViewById(R.id.textViewPhone)
+        val intent = intent
+        val name = intent.getStringExtra("namess")
+//        val email = intent.getStringExtra("emailss")
+//        val phone = intent.getLongExtra("phoness", 0)
+        print("======$name======")
+//        print("======$email======")
+//        print("======$phone======")
+        tvName.text = "Hello $name"
+//        tvEmail.text = "Your Email Address is: $email"
+//        tvPhone.text = "Your Phone Number is: $phone"
+
+    }
+}
