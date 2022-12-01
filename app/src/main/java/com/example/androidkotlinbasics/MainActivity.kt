66c947a7338883b801d0bc89c1c410518b6a11b8
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-01 20:06:47 +0530

    sending email from inside app

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.androidkotlinbasics
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidkotlinbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+
+        setContentView(view)
+
+        mainBinding.buttonSend.setOnClickListener {
+
+            val userAddress = mainBinding.editTextEmailAddress.text.toString()
+            val userSubject = mainBinding.editTextSubject.text.toString()
+            val userMessage = mainBinding.editTextMessage.text.toString()
+            sendEmail(userAddress, userSubject, userMessage)
+        }
     }
+
+    fun sendEmail(userAddress: String, userSubject: String, userMessage: String) {
+
+        val emailAddress = arrayOf(userAddress)
+        val emailIntent = Intent(Intent.ACTION_SENDTO)
+        emailIntent.data = Uri.parse("mailto:")
+
+//        val emailIntent = Intent(Intent.ACTION_SEND)
+//emailIntent.type = "text/plain"
+        //emailIntent.type = "*/*"
+//in manifest change android:name="android.intent.action.SEND"
+        //android:mimeType = */* // add this line between
+//this will show all applications as alternative for sending email
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, emailAddress)
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, userSubject)
+        emailIntent.putExtra(Intent.EXTRA_TEXT, userMessage)
+
+        if (emailIntent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(emailIntent, "Choose an App"))
+        }
+
+    }
+
 }
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+//Here we are using mail on Android Kotlin
