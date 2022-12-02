2bb8398bbb010bfa0a1e00bbba6e9aad665116fc
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-02 11:25:25 +0530

    making calls in android

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.androidkotlinbasics
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var phone: EditText
+    lateinit var call: Button
+    var userNumber: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        call = findViewById(R.id.buttonCall)
+        phone = findViewById(R.id.editTextPhone)
+
+        call.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    100
+                )
+
+            } else {
+                val intent = Intent(Intent.ACTION_CALL)
+                intent.data = Uri.parse("tel:$userNumber")
+                startActivity(intent)
+            }
+
+//            userNumber = phone.text.toString()
+//            val intent = Intent(Intent.ACTION_CALL)
+//            intent.data = Uri.parse("tel:$userNumber")
+//            startActivity(intent)
+
+        }
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$userNumber")
+            startActivity(intent)
+        }
+    }
+
 }
