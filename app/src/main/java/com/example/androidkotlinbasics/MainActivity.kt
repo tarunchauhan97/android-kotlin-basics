2d07cc463c98e782224ff121aa9f863f955c7121
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-11 20:28:11 +0530

    countries recycler list view basic

--- app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinbasics/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.androidkotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    var countryNameList = ArrayList<String>()
+    var detailsList = ArrayList<String>()
+    var imageList = ArrayList<Int>()
+
+    lateinit var adaptor: CountriesAdaptor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
+//        recyclerView.layoutManager = GridLayoutManager(this@MainActivity)
+
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        countryNameList.add("India")
+        countryNameList.add("Russia")
+        countryNameList.add("Israel")
+
+        detailsList.add("India is Vibrant")
+        detailsList.add("Russia is Monarch")
+        detailsList.add("Israel is Hardworking")
+
+        imageList.add(R.drawable.myimage)
+        imageList.add(R.drawable.login)
+        imageList.add(R.drawable.splash)
+        adaptor = CountriesAdaptor(countryNameList, detailsList, imageList, this@MainActivity)
+
+        recyclerView.adapter = adaptor
+
     }
 }
